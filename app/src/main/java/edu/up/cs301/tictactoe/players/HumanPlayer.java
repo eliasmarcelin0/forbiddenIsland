4448d7ad730cc451aaf0a2fe0e1dc47696964896
dockter25 <dockter25@up.edu>
2023-10-29 23:15:54 -0700

    added onclick method

--- app/src/main/java/edu/up/cs301/tictactoe/players/HumanPlayer.java
+++ app/src/main/java/edu/up/cs301/tictactoe/players/HumanPlayer.java
@@ -5,10 +5,12 @@ import android.view.View;
 import edu.up.cs301.game.GameFramework.GameMainActivity;
 import edu.up.cs301.game.GameFramework.infoMessage.GameInfo;
 import edu.up.cs301.game.GameFramework.players.GameHumanPlayer;
+import edu.up.cs301.tictactoe.infoMessage.GameState;
+
 import android.widget.EditText;
 
 
-public class HumanPlayer extends GameHumanPlayer {
+public class HumanPlayer extends GameHumanPlayer implements View.OnClickListener {
     /**
      * constructor
      *
@@ -32,4 +34,31 @@ public class HumanPlayer extends GameHumanPlayer {
     public void setAsGui(GameMainActivity activity) {
 
     }
+/*
+    @Override
+    public void onClick(View view) {
+        //clear text from multiline edit text
+        GameState firstInstance = new GameState();
+        GameState secondInstance = new = GameState(firstInstance);
+        firstInstance.move();
+        //multiline edit text about move
+        //(e.g., “Player 1 has moved his pawn from position 10 to position 14.”
+        //edit text = edit text + ;
+        firstInstance.shoreUp();
+        //edit text = edit text + ;
+        firstInstance.captureTreasure();
+        //edit text = edit text + ;
+        firstInstance.giveCard();
+        //edit text = edit text + ;
+        firstInstance.drawTreasure();
+        //edit text = edit text + ;
+        firstInstance.drawFlood();
+        //edit text = edit text + ;
+        GameState thirdInstance = new GameState();
+        GameState fourthInstance = new GameState();
+        fourthInstance = GameState(thirdInstance);
+        toString(thirdInstance);
+        toString(fourthInstance);
+        //edit text = edit text + ;
+    }*/
 }
