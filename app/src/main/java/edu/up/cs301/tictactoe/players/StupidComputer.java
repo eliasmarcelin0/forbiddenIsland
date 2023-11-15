4bb1560f1a3e24a6b372bf5b415f5a73ac528043
dockter25 <dockter25@up.edu>
2023-11-15 11:50:41 -0800

    working on stupidComputer ai

--- app/src/main/java/edu/up/cs301/tictactoe/players/StupidComputer.java
+++ app/src/main/java/edu/up/cs301/tictactoe/players/StupidComputer.java
@@ -3,6 +3,7 @@ package edu.up.cs301.tictactoe.players;
 import edu.up.cs301.game.GameFramework.infoMessage.GameInfo;
 import edu.up.cs301.game.GameFramework.infoMessage.GameState;
 import edu.up.cs301.game.GameFramework.players.GameComputerPlayer;
+import edu.up.cs301.tictactoe.infoMessage.FiGameState;
 
 public class StupidComputer extends GameComputerPlayer {
 
@@ -19,7 +20,25 @@ public class StupidComputer extends GameComputerPlayer {
 
     @Override
     protected void receiveInfo(GameInfo info) {
-        gameState = (GameState) info;
-
+        if(info instanceof FiGameState) {
+            FiGameState gameState = (FiGameState) info;
+            if(this.playerNum == gameState.getPlayerId()){
+                game.sendAction(gameState.drawTreasure());
+                int randomNum = (int) (Math.random() * 4);
+                if(randomNum == 0){
+                    game.sendAction(gameState.move());
+                }
+                else if(randomNum == 1){
+                    game.sendAction(gameState.shoreUp());
+                }
+                else if(randomNum == 2){
+                    game.sendAction(gameState.captureTreasure());
+                }
+                else {
+                    game.sendAction(gameState.giveCard());
+                }
+                game.sendAction(gameState.drawFlood());
+            }
+        }
     }
 }
