85ce874136a2f1417e6cde4d972834efb3ec2a30
deanteagleham <agleham27@up.edu>
2023-11-20 16:12:44 -0800

    FIButton comment

--- app/src/main/java/edu/up/cs301/tictactoe/animation/FIButton.java
+++ app/src/main/java/edu/up/cs301/tictactoe/animation/FIButton.java
@@ -3,6 +3,7 @@ package edu.up.cs301.tictactoe.animation;
 import android.content.Context;
 import android.widget.Button;
 
+//finds coordinates of the buttons
 public class FIButton extends Button {
     int x;
     int y;
